21cd870bfa04bc3e4839acd334186e94c1a4a467
ahmar-js <ahmar@rapidlabs.ai>
2024-04-24 00:44:31 +0500

    Add: Logger for Auth

--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth import authenticate
 from django.conf import settings
 from django.middleware import csrf
@@ -5,6 +7,7 @@ from rest_framework import exceptions as rest_exceptions, response, decorators a
 from rest_framework_simplejwt import tokens, views as jwt_views, serializers as jwt_serializers, exceptions as jwt_exceptions
 from user import serializers, models
 
+logger = logging.getLogger(__name__)
 
 def get_user_tokens(user):
     refresh = tokens.RefreshToken.for_user(user)
@@ -49,8 +52,8 @@ def loginView(request):
         res.data = tokens
         res["X-CSRFToken"] = csrf.get_token(request)
         return res
-    raise rest_exceptions.AuthenticationFailed(
-        "Email or Password is incorrect!")
+    logger.warn("Login failed for email: %s", email)
+    raise rest_exceptions.AuthenticationFailed("Email or Password is incorrect!")
 
 
 @rest_decorators.api_view(["POST"])
@@ -63,6 +66,7 @@ def registerView(request):
 
     if user is not None:
         return response.Response("Registered!")
+    logger.warn("Registration failed for email: %s", serializer.validated_data["email"])
     return rest_exceptions.AuthenticationFailed("Invalid credentials!")
 
 
@@ -80,10 +84,11 @@ def logoutView(request):
         res.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
         res.delete_cookie("X-CSRFToken")
         res.delete_cookie("csrftoken")
-        res["X-CSRFToken"]=None
-        
+        res["X-CSRFToken"] = None
+
         return res
     except:
+        logger.warn("Invalid token")
         raise rest_exceptions.ParseError("Invalid token")
 
 
