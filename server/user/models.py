61a2309cfef62b7e4ba143934878b963d2bf11ad
osman-y <osman.y@rapidlabs.ai>
2024-04-25 02:37:57 +0500

    Update: models to add role field

--- server/user/models.py
+++ server/user/models.py
@@ -4,30 +4,21 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
-
         if not email:
             raise ValueError("Email is required")
-
         user = self.model(
             email=self.normalize_email(email)
         )
-
         user.set_password(password)
         user.save(using=self._db)
-
         return user
-
-
     def create_superuser(self, email, password, **kwargs):
         user = self.create_user(
             email=self.normalize_email(email),
             password=password
         )
-
         user.first_name = kwargs.get('first_name')
         user.last_name = kwargs.get('last_name')
         user.is_admin = True
@@ -35,8 +26,6 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return
-
-
 class User(AbstractBaseUser):
     email = models.EmailField(null=False, blank=False, unique=True)
     first_name = models.CharField(max_length=50, blank=False, null=False, default='Default first name')
@@ -47,34 +36,26 @@ class User(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    role = models.CharField(max_length=50, blank=True, null=True)
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
     def __str__(self):
         return self.email
-
     def has_perm(self, perm, obj=None):
         return True
-
     def has_module_perms(self, app_label):
         return True
-    
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile/', default='media/profile/avatar.png')
     about = models.TextField(blank=True, null=True)
-
     def __str__(self):
         return f'{self.user.email} Profile'
-
 @receiver(post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
 @receiver(post_save, sender=get_user_model())
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
