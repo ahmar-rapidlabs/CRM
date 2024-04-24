35bbff75fcc57e2c3d1fc634986a373d14247acf
osman-y <osman.y@rapidlabs.ai>
2024-04-25 02:37:15 +0500

    Added: print user role

--- client/src/pages/auth/User.jsx
+++ client/src/pages/auth/User.jsx
@@ -3,32 +3,25 @@ import { useNavigate } from "react-router-dom"
 import useAuth from '../../hooks/useAuth'
 import useLogout from "../../hooks/useLogout"
 import useUser from '../../hooks/useUser'
-
 export default function User() {
-
     const { user } = useAuth()
-
     const navigate = useNavigate()
     const logout = useLogout()
     const [loading, setLoading] = useState(false)
     const getUser = useUser()
-
     useEffect(() => {
         getUser()
     }, [])
-
-
     async function onLogout() {
         setLoading(true)
-
         await logout()
         navigate('/')
     }
-    
     return (
         <div>
             <h4>{user?.id}</h4>
             <h4>{user?.username}</h4>
+            <h4>{user?.role}</h4>
             <h4>{user?.email}</h4>
             <h4>{user?.first_name}</h4>
             <h4>{user?.last_name}</h4>
